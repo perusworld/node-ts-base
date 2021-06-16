22123b40ffebfe3bae06b3b05003941c1b6af1bc
Saravana Perumal Shanmugam <perusworld@linux.com>
2021-06-16 07:25:22 -0700

    version upgrade

--- dist/impl.js
+++ dist/impl.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SomeInterfaceImpl = void 0;
 class SomeInterfaceImpl {
     sayHi(str) {
         return `Hello, ${str}`;
